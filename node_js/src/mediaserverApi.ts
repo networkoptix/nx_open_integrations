618c889c6abab6bfa3e37f062be2ce7d2e099afe
Mike Shevchenko <mshevchenko@networkoptix.com>
2019-06-13 21:46:03 +0100

    Updated copyright notices; added MPL 2.0 license text
    <==== Mercurial data ====>
    Branch: master

--- node_js/src/mediaserverApi.ts
+++ node_js/src/mediaserverApi.ts
@@ -1,9 +1,5 @@
-/*
- * Copyright 2017-present Network Optix, Inc.
- * This source code file is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at http://mozilla.org/MPL/2.0/.
- */
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
 import * as request from 'request-promise';
 import { BaseEvent, Rule } from './index';
 import { factory } from './logConfig';
