b1b1b4176dc67c35c67eeeaf01d1933635a70494
Andrey Terentyev <aterentyev@networkoptix.com>
2021-11-12 14:33:21 +0300

    make it capable of adding a RTSP stream

--- python/examples/add_camera_digest.py
+++ python/examples/add_camera_digest.py
@@ -25,25 +25,31 @@ def request_api(url, uri, method, **kwargs):
         return response.content
 
 def main():
-
+    is_stream = False
     parser = ArgumentParser()
     parser.add_argument("camera_data", 
-        help="A string containing credentials and an address of a camera in the format <username>:<password>@<address>:<port>")
+        help="A string containing credentials and an address of a camera in the format <username>:<password>@<address>:<port> or <username>:<password>@<RTSP url>")
     parser.add_argument("server_data",
         help="A string containing credentials and an address of a server in the format <username>:<password>@<address>:<port>")
     args = parser.parse_args()
-
-    camera_creds, camera_address = args.camera_data.split("@")
-    camera_ip, camera_port = camera_address.split(":")
-    camera_user, camera_password = camera_creds.split(":")
-
+    
     server_creds, server_address = args.server_data.split("@")
     server_url = f'https://{server_address}'
     username, password = server_creds.split(":")
     
-    api_url = f'/api/manualCamera/search?start_ip={camera_ip}&user={camera_user}&password={camera_password}'
+    camera_creds, camera_address = args.camera_data.split("@")
+    if str(camera_address).startswith("rtsp://"):
+        is_stream = True
+    if not is_stream:
+        camera_ip, camera_port = camera_address.split(":")
+    camera_user, camera_password = camera_creds.split(":")
+
+    if is_stream:
+        api_uri = f'/api/manualCamera/search?url={camera_address}'
+    else:
+        api_uri = f'/api/manualCamera/search?start_ip={camera_ip}&port={camera_port}user={camera_user}&password={camera_password}'
     search_data = request_api(server_url,
-                            api_url, 
+                            api_uri, 
                             'GET', 
                             auth=HTTPDigestAuth(username, password),
                             verify=False)
@@ -61,30 +67,45 @@ def main():
         if time.time() - start_time > search_timeout:
             print("Timeout exeeded. No camera found.")
             exit(1)
-            
-    camera_data = dict(
-         user = camera_user,
-         password = camera_password,
-         cameras = [
-            dict(
-                uniqueId = search_status["reply"]["cameras"][0]["uniqueId"],
-                url = search_status["reply"]["cameras"][0]["url"],
-                manufacturer = search_status["reply"]["cameras"][0]["manufacturer"],
-            )
-         ]
-    )
-    add_status = request_api(server_url,
+ 
+    # adding a RTSP stream
+    # for RTSP stream parameter names and HTTP method differ
+    if is_stream:
+        stream_data = dict(
+            user = camera_user,
+            password = camera_password,
+            uniqueId0=search_status["reply"]["cameras"][0]["uniqueId"],
+            url0=search_status["reply"]["cameras"][0]["url"],
+            manufacturer0=search_status["reply"]["cameras"][0]["manufacturer"]
+        )
+        add_status = request_api(server_url,
+                                f'/api/manualCamera/add', 
+                                'GET', 
+                                auth=HTTPDigestAuth(username, password),
+                                params = stream_data,
+                                verify=False)       
+    else: # adding a camera
+        camera_data = dict(
+            user = camera_user,
+            password = camera_password,
+            cameras = [
+                dict(
+                    uniqueId = search_status["reply"]["cameras"][0]["uniqueId"],
+                    url = search_status["reply"]["cameras"][0]["url"],
+                    manufacturer = search_status["reply"]["cameras"][0]["manufacturer"],
+                )
+            ]
+        )
+        add_status = request_api(server_url,
                             f'/api/manualCamera/add', 
                             'POST', 
                             auth=HTTPDigestAuth(username, password),
                             data = json.dumps(camera_data),
                             verify=False)                          
-    
     stop_status = request_api(server_url,
                             f'/api/manualCamera/stop?uuid={search_data["reply"]["processUuid"]}', 
                             'GET', 
                             auth=HTTPDigestAuth(username, password),
-                            verify=False)                          
-
+                            verify=False)  
 if __name__ == '__main__':
     main()
