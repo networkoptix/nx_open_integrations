c561814418d3123adc94881df5ae80437e8810ff
Mark Altera <maltera@networkoptix.com>
2024-09-20 15:35:13 +0200

    VI-54: Update JS API example

--- javascript/js_api_examples/resources/src/index.ts
+++ javascript/js_api_examples/resources/src/index.ts
@@ -1,19 +1,21 @@
 // Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 import { SceneItemsController, LayoutSettingsController, AuthController, TabsController } from "./controllers";
 import { initResourcesUI, initSceneItemsUI, initTabsUi } from "./initHandlers";
-import { screenshot } from "./screenshot";
 
-// Show help text when "Allow using Client API" hasn't been enabled
-setTimeout(() => {
-  if (!window.vms?.tab) {
-    document.getElementsByTagName(
-      "body"
-    )[0].innerHTML = `<h1>Enable "Allow using Client API" under advanced web page settings to use test page</h1><img src="${screenshot}">`;
-  }
-}, 2500);
+function showBanner(visible) {
+  document.querySelector("#banner").style.display = visible ? "initial" : "none";
+  document.querySelectorAll("section").forEach(
+    section => section.style.display = visible ? "none" : "initial");
+}
+
+if (!window.isVmsApiEnabled) {
+  window.onload = () => showBanner(true);
+}
 
 // Entry point for the Client API. After this callback call all functionality is available.
 window.vmsApiInit = () => {
+  showBanner(false);
+
   window.sceneItemsController = new SceneItemsController();
   window.layoutSettingsController = new LayoutSettingsController();
   window.tabsController = new TabsController();
